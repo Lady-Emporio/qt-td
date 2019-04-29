ae5e9e006c6ba8c843aab13ec0fbec902ffb248a
Lady-Emporio <Lady-Emporio>
2019-04-29 22:00:48 +0300

    add move try 3

--- Items.py
+++ Items.py
@@ -1,14 +1,29 @@
 from PyQt5.QtWidgets import QGraphicsRectItem 
-from PyQt5.QtGui import QColor ,QFontMetrics 
+from PyQt5.QtGui import QColor ,QFontMetrics ,QVector2D
 from PyQt5.QtCore import Qt,QRectF
 class Unit(QGraphicsRectItem):
 	WIDTH=30
 	HEIGHT=30
+	PATH=[]
+	SPEED=0.3;
 	def __init__(self,x,y):
 		QGraphicsRectItem.__init__(self,0,0,self.WIDTH,self.HEIGHT)
 		self.hp=100
 		
 		self.setPos(x-self.WIDTH/2,y-self.HEIGHT/2)
+
+		self.point=0;
+		self.needGoTo=QVector2D(0,0)
+		self.moveX=0;
+		self.moveY=0;
+
+		self.actuallyX=self.x();
+		self.actuallyY=self.y();
+		
+		if self.point<=len(self.PATH)-1:
+			needGoTo=self.PATH[self.point]
+			self.initGoTo(needGoTo[0],needGoTo[1])
+
 	def paint(self,painter , StyleOptionGraphicsItem , Widget ):
 		painter.setBrush(QColor(0,128,0))
 		painter.drawRect(self.rect())
@@ -18,8 +33,63 @@ class Unit(QGraphicsRectItem):
 		rect=QRectF(0,0,self.WIDTH,self.HEIGHT)
 		painter.drawText(rect, Qt.AlignHCenter|Qt.AlignTop, str(self.hp))
 		
-	def mousePressEvent(self,QGraphicsSceneMouseEvent):
-		self.hp-=20
-		if(self.hp<=0):
-			self.scene().removeItem(self)
+	#def mousePressEvent(self,QGraphicsSceneMouseEvent):
+	#	self.hp-=20
+	#	if(self.hp<=0):
+	#		self.scene().removeItem(self)
+	#	self.update()
+	def initGoTo(self,x,y):
+
+		nowPosition=QVector2D(self.x(),self.y())
+		self.needGoTo=QVector2D(x-self.WIDTH/2,y-self.HEIGHT/2)
+		distance=nowPosition.distanceToPoint(self.needGoTo)
+		iterTimeToGoToGoal=distance/self.SPEED;
+
+		if iterTimeToGoToGoal==0:
+			self.moveX=0;
+			self.moveY=0;
+			return
+
+		needGo=self.needGoTo-nowPosition
+		needGoToX=needGo.x();
+		needGoToY=needGo.y();
+
+		self.moveX=needGoToX/iterTimeToGoToGoal
+		self.moveY=needGoToY/iterTimeToGoToGoal
+
+	def move(self):
+		if self.x()==self.needGoTo.x() and self.y()==self.needGoTo.y():
+			self.point+=1
+			if self.point<=len(self.PATH)-1:
+				needGoTo=self.PATH[self.point]
+				self.initGoTo(needGoTo[0],needGoTo[1])
+				
+
+		if self.moveX==0 and self.moveY==0:
+			return 
+		if self.moveX>0:
+			if self.actuallyX>=self.needGoTo.x():
+				self.actuallyX=self.needGoTo.x()
+				self.moveX=0
+		elif self.moveX<0:
+			if self.actuallyX<=self.needGoTo.x():
+				self.actuallyX=self.needGoTo.x()
+				self.moveX=0
+
+		if self.moveY>0:
+			if self.actuallyY>=self.needGoTo.y():
+				self.actuallyY=self.needGoTo.y()
+				self.moveY=0
+		elif self.moveY<0:
+			if self.actuallyY<=self.needGoTo.y():
+				self.actuallyY=self.needGoTo.y()
+				self.moveY=0
+
+
+		self.actuallyX+=self.moveX
+		self.actuallyY+=self.moveY
+		self.setPos(self.actuallyX,self.actuallyY)
 		self.update()
+		#print([self.actuallyX,self.actuallyY,self.moveX,self.moveY])
+		
+	
--- Scene.py
+++ Scene.py
@@ -1,21 +1,83 @@
 
-from PyQt5.QtWidgets import QGraphicsScene
+from PyQt5.QtWidgets import QGraphicsScene,QGraphicsEllipseItem,QGraphicsLineItem
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QColor,QBrush,QPen
 from Items import Unit
 class Scene(QGraphicsScene):
+	mobs=[]
 	def __init__(self):
 		QGraphicsScene.__init__(self)
-		WIDTH=1000
-		HEIGHT=1000
+		WIDTH=600
+		HEIGHT=600
 		self.setSceneRect(0, 0, WIDTH, HEIGHT)
 		
-		# self.addItem(Unit(100,100))
+		self.timer=QTimer()
+		self.timer.timeout.connect(self.timerWork)
+		self.timer.setInterval(16);
+		self.timer.start() 
+
+		
+		
+		
+
+		self.addPointToPath(100,300)
+		self.addPointToPath(300,200)
+		self.addPointToPath(150,50)
+		
+
+
+		u=Unit(30,30)
+		self.addItem(u)
+		self.mobs.append(u)
+
+
+		self.unit=QGraphicsEllipseItem(100, 100, 50, 50);
+		self.unit.setBrush(QColor(0,128,128))
+		self.addItem(self.unit);
+
 	def mousePressEvent(self,GraphicsSceneMouseEvent):
+		
 		PointF =GraphicsSceneMouseEvent.scenePos()
 		x=PointF.x()
 		y=PointF.y()
-		u=Unit(x,y)
-		self.addItem(u)
-		qlist_Elements=self.collidingItems(u)
-		if (len(qlist_Elements)!=0):
-			self.removeItem(u)
-			QGraphicsScene.mousePressEvent(self,GraphicsSceneMouseEvent)
+		##print([self.unit.rect(),x,y])
+		self.unit.setPos(x-125,y-125)
+		self.unit.update()
+		#u=Unit(x,y)
+		#self.addItem(u)
+
+
+		#self.mobs[0].initGoTo(x,y)
+	#	qlist_Elements=self.collidingItems(u)
+	#	if (len(qlist_Elements)!=0):
+	#		self.removeItem(u)
+	#		QGraphicsScene.mousePressEvent(self,GraphicsSceneMouseEvent)
+
+
+	def addPointToPath(self,x,y):
+		widthEllipse=20;
+		heightEllipse=20;
+		EllipseX=x-widthEllipse/2
+		EllipseY=y-heightEllipse/2
+
+		if(len(Unit.PATH)!=0):
+			lastPoint=Unit.PATH[len(Unit.PATH)-1]
+			line=QGraphicsLineItem(lastPoint[0],lastPoint[1],x,y)
+			line.setPen(QPen(QBrush(QColor(128,0,0)),5))
+			self.addItem(line);
+		Unit.PATH.append([x,y])	
+
+		item =QGraphicsEllipseItem(EllipseX, EllipseY, widthEllipse, heightEllipse);
+		item.setBrush(QColor(0,0,128))
+		self.addItem(item);
+
+		
+
+
+	def timerWork(self):
+		for mob in self.mobs:
+			if mob.collidesWithItem(self.unit):
+				print("collaps")
+				return
+			mob.move()
+			
